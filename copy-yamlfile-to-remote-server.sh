a8d668477fcdf7de5795778fb9fa15af3f540166
Shan-1028 <shielarosemarilao@gmail.com>
2026-06-03 14:49:13 +0800

    update sh file

--- copy-yamlfile-to-remote-server.sh
+++ copy-yamlfile-to-remote-server.sh
@@ -5,6 +5,7 @@ USER="ubuntu"
 SERVER="54.225.138.102"
 REMOTE_DIR="/home/ubuntu/"
 
+# copy folders recursively and files from the local server to the remote server
 scp -r -i terraform/key-pair/"$KEY" \
 docker-compose.yml prometheus/ \
 "$USER"@"$SERVER":"$REMOTE_DIR"
