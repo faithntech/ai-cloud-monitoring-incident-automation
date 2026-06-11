4d7ecaf69eda50cab8980f09c22233794ed43e47
Shan-1028 <shielarosemarilao@gmail.com>
2026-06-11 16:41:20 +0800

    update files

--- copy-yamlfile-to-remote-server.sh
+++ copy-yamlfile-to-remote-server.sh
@@ -2,7 +2,7 @@
 
 KEY="ai-cloud-key"
 USER="ubuntu"
-SERVER="3.214.165.220"
+SERVER="44.206.112.216"
 REMOTE_DIR="/home/ubuntu/"
 
 # copy folders recursively and files from the local server to the remote server
